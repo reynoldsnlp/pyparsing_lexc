5e2edd7474861cb3a149730d5e146fb9d7845e04
reynoldsnlp <reynoldsnlp@users.noreply.github.com>
2020-03-04 20:55:43 -0700

    pep 8

--- pyparsing_lexc/__init__.py
+++ pyparsing_lexc/__init__.py
@@ -3,9 +3,11 @@
 import pyparsing as pp
 
 # define punctuation and special words
-COLON,SEMI = map(pp.Suppress, ":;")
+COLON = pp.Suppress(':')
+SEMI = pp.Suppress(';')
 HASH = pp.Literal('#')
-LEXICON, END = map(pp.Keyword, "LEXICON END".split())
+LEXICON = pp.Keyword('LEXICON')
+END = pp.Keyword('END')
 
 # use regex and Combine to handle % escapes
 escaped_char = pp.Regex(r'%.').setParseAction(lambda t: t[0][1])
@@ -21,13 +23,14 @@ value_expr = ident()
 #    name value ;
 #    name value # ;
 lexicon_decl = pp.Group(ident("name")
-                     + pp.Optional(pp.Optional(COLON)
-                                + value_expr("value")
-                                + pp.Optional(HASH)('hash'))
-                     + SEMI)
+                        + pp.Optional(pp.Optional(COLON)
+                                      + value_expr("value")
+                                      + pp.Optional(HASH)('hash'))
+                        + SEMI)
 
-# use a parse action to normalize the parsed values
+
 def fixup_value(tokens):
+    """Use a parse action to normalize the parsed values."""
     if 'value' in tokens[0]:
         # pyparsing makes this a nested element, just take zero'th value
         if isinstance(tokens[0].value, pp.ParseResults):
@@ -35,21 +38,26 @@ def fixup_value(tokens):
     else:
         # no value was given, expand 'name' as if  parsed 'name:name'
         tokens[0]['value'] = tokens[0].name
+
+
 lexicon_decl.setParseAction(fixup_value)
 
-# define a whole LEXICON section
-# TBD - make name optional, define as 'Root'
-lexicon_section = pp.Group(LEXICON
-                        + ident("name")
-                        + pp.ZeroOrMore(lexicon_decl, stopOn=LEXICON | END)("declarations"))
 
-# this part still TBD - just put in a placeholder for now
+# define a whole LEXICON section
+# TODO make name optional, define as 'Root'
+lexicon_section = pp.Group(LEXICON
+                           + ident("name")
+                           + pp.ZeroOrMore(lexicon_decl,
+                                           stopOn=LEXICON | END)("declarations"))  # noqa: E501
+
+# TODO just put in a placeholder for now
 multichar_symbols_section = pp.empty()
 
 # tie it all together
 parser = (pp.Optional(multichar_symbols_section)('multichar_symbols')
           + pp.Group(pp.OneOrMore(lexicon_section))('lexicons')
           + END)
+
 
 # ignore comments anywhere in our parser
 comment = '!' + pp.Optional(pp.restOfLine)
